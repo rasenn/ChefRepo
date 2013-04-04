6e68a683c4d24b82a8a0e7ee99971732d39e5d0e
ando <ando@kraken.local>
2013-04-05 00:04:03 +0900

    before create recipe java

---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "git" do 
+	action :install
+end
+
+
+
+
+"git://github.com/opscode-cookbooks/java.git"
+ 
